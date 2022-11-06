d1bdcb8b7de1bad8cab8b2095099ffa0846da28a
Kilimanjaro874 <vantan2022.228995@gmail.com>
2022-11-07 04:05:36 +0900

    ランダム生成機能追加：一発でクリアできないよう工夫

--- Assets/CS_Class/22_1111_LightsOuts/LightsOuts.cs
+++ Assets/CS_Class/22_1111_LightsOuts/LightsOuts.cs
@@ -42,12 +42,10 @@ public class LightsOuts : MonoBehaviour, IPointerClickHandler
                 // --- 盤面の初期状態決定 --- //
                 scr.Row = r;                // 行代入
                 scr.Col = c;                // 列代入
-                // --- ランダム生成 --- //
-                scr.LightOn = false;        // 
-                col.color = _off_color;     // 色の決定
-
             }
         }
+        // --- ランダム生成 --- //
+        RandCellMake();
     }
 
     public void OnPointerClick(PointerEventData eventData)
@@ -114,8 +112,51 @@ public class LightsOuts : MonoBehaviour, IPointerClickHandler
         {
             for (var c = 0; c < _cellSt.GetLength(1); c++)
             {
-               
+                // -- 以下の一発でクリア出来ないように実行する処理のテスト -- //
+                // -> あえて全てのCellを、成功条件の白に変更する 
+                _cellSt[r,c]._scr.LightOn = true;           // LightOn = true
+                _cellSt[r, c]._img.color = _on_color;       // 白
+                int tempRand = Random.Range((int)0, (int)1);
+                //if(tempRand == 0)
+                //{
+                //    _cellSt[r, c]._scr.LightOn = false;             // LightOn = false
+                //    _cellSt[r, c]._img.color = _off_color;          // 黒
+                //}
             }
         }
+        // --- 一発でクリアできない用に走査を行う --- //
+        // 少なくとも、「白が十字で存在する場所が一か所も無い」なら大丈夫
+        // その部分があれば、その内1~2箇所黒く塗りつぶす
+        for (var r = 0; r < _cellSt.GetLength(0); r++)
+        {
+            int count_white = 0;
+            for (var c = 0; c < _cellSt.GetLength(1); c++)
+            {
+                if(_cellSt[r, c]._scr.LightOn) { count_white++; }
+                else { count_white = 0; }
+                if(count_white >= 2)
+                {
+                    // --- 2個白が横に連続：十字が存在しないかチェックしていく --- //
+                    if(c + 1 >= _cellSt.GetLength(1)) { continue; }     // 右は範囲外、OK
+                    if(!_cellSt[r,c + 1]._scr.LightOn) { continue; }    // 右は黒、OK 
+                    if (r - 1 < 0) { continue; }                        // 上は範囲外、OK
+                    if(!_cellSt[r-1, c]._scr.LightOn) { continue; }     // 上は黒、OK
+                    if(r + 1 >= _cellSt.GetLength(0)) { continue; }     // 下は範囲外、OK
+                    if(!_cellSt[r+1, c]._scr.LightOn) { continue; }     // 下は黒　OK
+                    // -- 白十字が発見された時の処理 -- //
+                    int tmp_roop = Random.Range((int)1, (int)3);
+                    for (int i = 0; i < tmp_roop; i++)
+                    {
+                        // - 白十字の内、数か所黒に変更 - //
+                        int tmp_r = Random.Range((int)-1, (int)1);
+                        int tmp_c = Random.Range((int)-1, (int)1);
+                        _cellSt[r + tmp_r, c + tmp_c]._scr.LightOn = false;
+                        _cellSt[r + tmp_r, c + tmp_c]._img.color = _off_color;
+                    }
+
+                }
+            }
+        }
+
     }
 }
