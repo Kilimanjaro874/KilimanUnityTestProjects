75a44637b59dd0c7ab5ea6403d11640e56f9d4ba
Kilimanjaro874 <vantan2022.228995@gmail.com>
2023-01-20 23:54:30 +0900

    Fix: [PiceStocker] Repar GameObject and Code.

--- Assets/CS_Class/23_0120_Reversi/Script/PiceManager.cs
+++ Assets/CS_Class/23_0120_Reversi/Script/PiceManager.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class PiceManager : MonoBehaviour
@@ -7,13 +8,23 @@ public class PiceManager : MonoBehaviour
     //// ---- Memver variables ---- ////
     // --- Define --- //
     const int PieceNum_ = 32;
-    const float PieceSpan_ = 0.4f;
+    const float PieceSpan_ = 0.15f;
     [SerializeField]
-    private Transform PieceGenPos;
+    private Transform PieceGenTransform_;
     [SerializeField]
     GameObject Piece_;
-    
-    
-   
+
+    private void Start()
+    {
+        for(var c = 0; c < PieceNum_; c++)
+        {
+            var piece = Instantiate(Piece_);
+            piece.transform.SetParent(PieceGenTransform_);
+           // piece.transform.position = PieceGenTransform_.position;
+            //Vector3 tmp = PieceGenTransform_.position;
+            piece.transform.position = PieceGenTransform_.position + PieceGenTransform_.forward * (float)c * PieceSpan_;
+        }
+    }
+
 
 }
