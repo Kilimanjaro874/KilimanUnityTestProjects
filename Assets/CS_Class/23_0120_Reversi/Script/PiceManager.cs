543f4bf4ea2036864ab4254f9399bb33717e3a21
Kilimanjaro874 <vantan2022.228995@gmail.com>
2023-01-20 23:24:29 +0900

    Add: [PiceStocker] make objects.

---
+++ Assets/CS_Class/23_0120_Reversi/Script/PiceManager.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PiceManager : MonoBehaviour
+{
+    //// ---- Memver variables ---- ////
+    // --- Define --- //
+    const int PieceNum_ = 32;
+    const float PieceSpan_ = 0.4f;
+    [SerializeField]
+    private Transform PieceGenPos;
+    [SerializeField]
+    GameObject Piece_;
+    
+    
+   
+
+}
